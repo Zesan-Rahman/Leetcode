f21d022189868ec83ec62a0a9b3af5340c0160f8
Zesan Rahman <ramenzzz64@gmail.com>
2025-05-25 14:40:37 -0400

    Completed problem 2942, forgot to push

---
+++ Summer2025/2942.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> result;
+        for (int i = 0; i < words.size(); i++) {
+            if (words[i].find(x) != string::npos) {
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
