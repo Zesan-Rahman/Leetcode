15c1c2f8b2029169f084b465e737554eee5ddb0d
Zesan Rahman <ramenzzz64@gmail.com>
2025-06-03 19:26:24 -0400

    Completed 1298

--- Summer2025/1298.cpp
+++ Summer2025/1298.cpp
@@ -18,34 +18,43 @@ public:
             vector<int> unlockable = status;
             for (size_t i = 0; i < initialBoxes.size(); i++) {
                 int rootBox = initialBoxes[i];
-                if(visited[rootBox] == -1) visited[rootBox] = 0;
-                if(unlockable[rootBox] == 1) {
+                if(visited[rootBox] != 1) {
                     queue<int> q;
                     q.push(rootBox);
                     while(!q.empty()) {
                         int currentBox = q.front();
                         q.pop();
-                        visited[currentBox] = 0;
-                        if(unlockable[currentBox]) {
-                            visited[currentBox] = 1;
-                            for(int k = 0; k < keys[currentBox].size(); k++) {
-                                //Key also functions as box number
-                                int key = keys[currentBox][k];
-                                unlockable[key] = 1;
-                                if(visited[key] == 0){
-                                    q.push(key);
-                                }
+                        if(visited[currentBox] != 1) {
+                            if(!unlockable[currentBox]) {
+                                visited[currentBox] = 0;
                             }
-                            candyCount += candies[currentBox];
-                            for (int j = 0; j < containedBoxes[currentBox].size(); j++) {
-                                if(visited[containedBoxes[currentBox][j]] == -1) {
-                                    q.push(containedBoxes[currentBox][j]);
-                                }   
+                            else{ 
+                                visited[currentBox] = 1;
+                                candyCount += openBox(candies, keys, unlockable, containedBoxes, visited, q, currentBox);
                             }
                         }
                     }
                 }
             }
             return candyCount;
+    }
+
+    int openBox(vector<int>& candies, vector<vector<int>>& keys, vector<int>& unlockable, vector<vector<int>>& containedBoxes,
+    vector<int>& visited, queue<int>& q, int currentBox) 
+    {
+        for(int k = 0; k < keys[currentBox].size(); k++) {
+            //Key also functions as box number
+            int key = keys[currentBox][k];
+            unlockable[key] = 1;
+            if(visited[key] == 0){
+                q.push(key);
+            }
+        }
+        for (int j = 0; j < containedBoxes[currentBox].size(); j++) {
+            if(visited[containedBoxes[currentBox][j]] == -1) {
+                q.push(containedBoxes[currentBox][j]);
+            }   
+        }
+        return candies[currentBox];;
     }
 };
